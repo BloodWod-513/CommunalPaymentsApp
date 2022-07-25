be367bca055961d5a56e1c4b09f7d2107e81cee9
BloodWod-513 <dayn_2013@mail.ru>
2022-07-25 22:24:27 +0500

    add cold water tariffs

---
+++ CommunalPaymentsApp/MVVM/Model/ColdWaterGeneralTariffParameter.cs
@@ -0,0 +1,17 @@
+﻿using CommunalPaymentsApp.MVVM.Model.AbstractServiceParameter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunalPaymentsApp.MVVM.Model
+{
+    public class ColdWaterGeneralTariffParameter : TariffServiceParameter
+    {
+        public ColdWaterGeneralTariffParameter(double serviceCost, double previousSevriceCost) : base(serviceCost, previousSevriceCost)
+        {
+            Tariff = CloneTariff(Tariff.TypeTariff.ColdWater);
+        }
+    }
+}
---
+++ CommunalPaymentsApp/MVVM/Model/ColdWaterNormativeParameter.cs
@@ -0,0 +1,17 @@
+﻿using CommunalPaymentsApp.MVVM.Model.AbstractServiceParameter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunalPaymentsApp.MVVM.Model
+{
+    public class ColdWaterNormativeParameter : NormativeServiceParameter
+    {
+        public ColdWaterNormativeParameter(int numberOfResidents) : base(numberOfResidents, Tariff.TypeTariff.ColdWater)
+        {
+            Tariff = CloneTariff(Tariff.TypeTariff.ColdWater);
+        }
+    }
+}
