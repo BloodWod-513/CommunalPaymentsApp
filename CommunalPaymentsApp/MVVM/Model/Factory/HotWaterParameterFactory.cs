62ef2b30de0b86831eca3c6a2eb5918c048c7167
BloodWod-513 <dayn_2013@mail.ru>
2022-07-25 22:35:11 +0500

    add hot water factory

---
+++ CommunalPaymentsApp/MVVM/Model/Factory/HotWaterParameterFactory.cs
@@ -0,0 +1,17 @@
+﻿using CommunalPaymentsApp.MVVM.Model.AbstractServiceParameter;
+using CommunalPaymentsApp.MVVM.Model.Interface;
+namespace CommunalPaymentsApp.MVVM.Model.Factory
+{
+    public class HotWaterParameterFactory : IServiceFactory
+    {
+        public NormativeServiceParameter CreateNormativeServiceParameter(int numberOfResidents)
+        {
+            return new HotWaterNormativeParameter(numberOfResidents);
+        }
+
+        public TariffServiceParameter CreateGeneralTariffServiceParameter(double serviceCost, double previousSevriceCost)
+        {
+            return new HotWaterGeneralTariffParameter(serviceCost, previousSevriceCost);
+        }
+    }
+}
