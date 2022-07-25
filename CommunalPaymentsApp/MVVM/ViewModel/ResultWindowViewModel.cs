72c7384834faa6c8ef4500f26cbefbef3e674afd
BloodWod-513 <dayn_2013@mail.ru>
2022-07-25 22:40:06 +0500

    Add ResultWindowViewModel

---
+++ CommunalPaymentsApp/MVVM/ViewModel/ResultWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using CommunalPaymentsApp.MVVM.Model;
+using CommunalPaymentsApp.MVVM.Model.AbstractServiceParameter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunalPaymentsApp.MVVM.ViewModel
+{
+    public class ResultWindowViewModel
+    {
+        public List<ServiceParameter?> ServiceParameters { get; set; }
+        public ResultWindowViewModel(List<ServiceParameter?> serviceParameters)
+        {
+            ServiceParameters = serviceParameters;
+        }
+
+    }
+}
