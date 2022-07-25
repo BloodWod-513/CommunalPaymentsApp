f88c51ddd4002cf99c8ea993a5de68d52be3dbcf
BloodWod-513 <dayn_2013@mail.ru>
2022-07-25 22:25:16 +0500

    add hot water tariffs

---
+++ CommunalPaymentsApp/MVVM/Model/HotWaterGeneralTariffParameter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunalPaymentsApp.MVVM.Model
+{
+    internal class HotWaterGeneralTariffParameter : AbstractServiceParameter.TariffServiceParameter
+    {
+        public HotWaterGeneralTariffParameter(double serviceCost, double previousSevriceCost) : base(serviceCost, previousSevriceCost)
+        {
+            Tariff = CloneTariff(Tariff.TypeTariff.HotWater);
+        }
+    }
+}
---
+++ CommunalPaymentsApp/MVVM/Model/HotWaterNormativeParameter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunalPaymentsApp.MVVM.Model
+{
+    public class HotWaterNormativeParameter : AbstractServiceParameter.NormativeServiceParameter
+    {
+        public HotWaterNormativeParameter(int numberOfResidents) : base(numberOfResidents, Tariff.TypeTariff.HotWater)
+        {
+            Tariff = CloneTariff(Tariff.TypeTariff.HotWater);
+        }
+    }
+}
