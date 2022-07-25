0ea9f9c727914ae5ea20c1a7722a95c5425d578b
BloodWod-513 <dayn_2013@mail.ru>
2022-07-25 22:34:43 +0500

    Add cold water factory

---
+++ CommunalPaymentsApp/MVVM/Model/Factory/ColdWaterParameterFactory.cs
@@ -0,0 +1,23 @@
+﻿using CommunalPaymentsApp.MVVM.Model.Interface;
+using CommunalPaymentsApp.MVVM.Model.AbstractServiceParameter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunalPaymentsApp.MVVM.Model.Factory
+{
+    public class ColdWaterParameterFactory : IServiceFactory
+    {
+        public NormativeServiceParameter CreateNormativeServiceParameter(int numberOfResidents)
+        {
+            return new ColdWaterNormativeParameter(numberOfResidents);
+        }
+
+        public TariffServiceParameter CreateGeneralTariffServiceParameter(double serviceCost, double previousSevriceCost)
+        {
+            return new ColdWaterGeneralTariffParameter(serviceCost, previousSevriceCost);
+        }
+    }
+}
