22dc3f2c27b8e77fd0bc660ed10c98c0378f0c61
BloodWod-513 <dayn_2013@mail.ru>
2022-07-25 22:35:01 +0500

    add electrocity factory

---
+++ CommunalPaymentsApp/MVVM/Model/Factory/ElectrocityParameterFactory.cs
@@ -0,0 +1,28 @@
+﻿using CommunalPaymentsApp.MVVM.Model.AbstractServiceParameter;
+using CommunalPaymentsApp.MVVM.Model.Interface;
+
+namespace CommunalPaymentsApp.MVVM.Model.Factory
+{
+    public class ElectrocityParameterFactory : IServiceFactory, IServiceDayTariffFactory, IServiceNightTariffFactory
+    {
+        public NormativeServiceParameter CreateNormativeServiceParameter(int numberOfResidents)
+        {
+            return new ElectrocityNormativeParameter(numberOfResidents);
+        }
+
+        public TariffServiceParameter CreateGeneralTariffServiceParameter(double serviceCost, double previousSevriceCost)
+        {
+            return new ElectrocityGeneralTariffParameter(serviceCost, previousSevriceCost);
+        }
+
+        public TariffServiceParameter CreateDayTariffServiceParameter(double serviceCost, double previousSevriceCost)
+        {
+            return new ElectrocityPerDayTariffParameter(serviceCost, previousSevriceCost);
+        }
+
+        public TariffServiceParameter CreateNightTariffServiceParameter(double serviceCost, double previousSevriceCost)
+        {
+            return new ElectrocityPerNightTariffParameter(serviceCost, previousSevriceCost);
+        }
+    }
+}
