6150c601bd2d6213f379be44abeb92376c643d40
BloodWod-513 <dayn_2013@mail.ru>
2022-07-25 22:18:50 +0500

    Add tariffs dbset and startup parameters

--- CommunalPaymentsApp/ApplicationContext.cs
+++ CommunalPaymentsApp/ApplicationContext.cs
@@ -10,7 +10,7 @@ namespace CommunalPaymentsApp
 {
     internal class ApplicationContext : DbContext
     {
-        public DbSet<Tariff> Tariffs => Set<Tariff>();
+        public DbSet<Tariff> Tariffs { get; set; } = null!;
         public ApplicationContext()
         {
             Database.EnsureDeleted();
@@ -26,13 +26,13 @@ namespace CommunalPaymentsApp
             modelBuilder.Entity<Tariff>().HasData(
                 new Tariff[]
                 {
-                    new Tariff("ХВС", 35.78, 4.85, Tariff.UnitsMesurement.CubicMeter),
-                    new Tariff("ГВС", 158.98, 4.01, Tariff.UnitsMesurement.CubicMeter),
-                    new Tariff("ЭЭ", 4.28, 164, Tariff.UnitsMesurement.Kilowatt),
-                    new Tariff("ЭЭ день", 4.9, null, Tariff.UnitsMesurement.Kilowatt),
-                    new Tariff("ЭЭ ночь", 2.31, null, Tariff.UnitsMesurement.Kilowatt),
-                    new Tariff("ГВС Теплоноситель", 35.38, 4.01, Tariff.UnitsMesurement.CubicMeter),
-                    new Tariff("ГВС Тепловая энергия", 998.69, 0.05349, Tariff.UnitsMesurement.Gigacalories),
+                    new Tariff("ХВС", 35.78, 4.85, Tariff.UnitsMesurement.CubicMeter, Tariff.TypeTariff.ColdWater) { Id = 0 },
+                    new Tariff("ГВС", 158.98, 4.01, Tariff.UnitsMesurement.CubicMeter, Tariff.TypeTariff.HotWater) { Id = 1 },
+                    new Tariff("ЭЭ", 4.28, 164, Tariff.UnitsMesurement.Kilowatt, Tariff.TypeTariff.Electricity) { Id = 2 },
+                    new Tariff("ЭЭ день", 4.9, null, Tariff.UnitsMesurement.Kilowatt, Tariff.TypeTariff.ElectricityPerDay) { Id = 3 },
+                    new Tariff("ЭЭ ночь", 2.31, null, Tariff.UnitsMesurement.Kilowatt, Tariff.TypeTariff.ElectricityPerNight) { Id = 4 },
+                    new Tariff("ГВС Теплоноситель", 35.38, 4.01, Tariff.UnitsMesurement.CubicMeter, Tariff.TypeTariff.HotWaterOfHeatCarrier) { Id = 5 },
+                    new Tariff("ГВС Тепловая энергия", 998.69, 0.05349, Tariff.UnitsMesurement.Gigacalories, Tariff.TypeTariff.HotWaterThermalEnergy) { Id = 6 },
                 });
         }
     }
