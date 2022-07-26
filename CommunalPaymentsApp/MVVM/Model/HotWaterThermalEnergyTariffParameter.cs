40e2a1ed8dca0a77f6c97dbae8691d7896a91dba
BloodWod-513 <dayn_2013@mail.ru>
2022-07-26 13:51:11 +0500

    add HotWaterThermalEnergyTariffParameter

---
+++ CommunalPaymentsApp/MVVM/Model/HotWaterThermalEnergyTariffParameter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunalPaymentsApp.MVVM.Model
+{
+    public class HotWaterThermalEnergyTariffParameter : AbstractServiceParameter.ServiceParameter
+    {
+        public HotWaterThermalEnergyTariffParameter(double volumeOfHotWater) : base(Tariff.TypeTariff.HotWaterThermalEnergy)
+        {
+            if (Tariff?.Normative != null)
+                VolumeOfService = (double)(volumeOfHotWater * Tariff.Normative);
+        }
+    }
+}
