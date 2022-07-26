115cb78ca52a9d3c2df15a3d867578818e0ffae5
BloodWod-513 <dayn_2013@mail.ru>
2022-07-26 18:31:44 +0500

    refactoring MainWindow for validation

--- CommunalPaymentsApp/MVVM/View/MainWindow.xaml.cs
+++ CommunalPaymentsApp/MVVM/View/MainWindow.xaml.cs
@@ -1,21 +1,11 @@
 ﻿using CommunalPaymentsApp.MVVM.ViewModel;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 using CommunalPaymentsApp.MVVM.Model;
 using CommunalPaymentsApp.MVVM.Model.Factory;
 using CommunalPaymentsApp.MVVM.Model.AbstractServiceParameter;
+using CommunalPaymentsApp.ValidationTools;
 
 namespace CommunalPaymentsApp.MVVM.View
 {
@@ -35,15 +25,15 @@ namespace CommunalPaymentsApp.MVVM.View
         }
         private void MakeAccrualsButton_Click(object sender, RoutedEventArgs e)
         {
-            if (!TryEnterInt(AmountOfResidents.TextBox.Text))
+            if (!DigitValidation.TryEnterInt(AmountOfResidents.TextBox.Text, "Число жильцов"))
                 return;
             int numberOfResidents = int.Parse(AmountOfResidents.TextBox.Text);
 
-            if (!TryEnterDouble(Debt.TextBox.Text))
+            if (!DigitValidation.TryEnterDouble(Debt.TextBox.Text, "Долг"))
                 return;
             double debt = double.Parse(Debt.TextBox.Text); 
 
-            if (!TryEnterDouble(Payment.TextBox.Text))
+            if (!DigitValidation.TryEnterDouble(Payment.TextBox.Text, "Плата"))
                 return;
             double payment = double.Parse(Payment.TextBox.Text);
             debt -= payment;
@@ -55,9 +45,11 @@ namespace CommunalPaymentsApp.MVVM.View
                 electricyServiceParameter = null, electricyPerDayServiceParameter = null, electricyPerNightServiceParameter = null;
             if ((bool)ColdWater.CheckBox.IsChecked)
             {
-                coldWaterServiceParameter = ServiceParameterCreator.CreateTariffParameter(new ColdWaterParameterFactory(), ColdWater.Value, ColdWater.PrevValue);
-                isCorrectParams = CurMoreThanPrevParameter(ColdWater.PrevValue, ColdWater.Value);
+                if (!DigitValidation.TryEnterDouble(ColdWater.Value.ToString(), "Холодная вода текущий") || !DigitValidation.TryEnterDouble(ColdWater.PrevValue.ToString(), "Холодная вода предыдущий"))
+                    return;
+                isCorrectParams = DigitValidation.CurMoreThanPrevParameter(ColdWater.PrevValue, ColdWater.Value);
                 if (!isCorrectParams) return;
+                coldWaterServiceParameter = ServiceParameterCreator.CreateTariffParameter(new ColdWaterParameterFactory(), ColdWater.Value, ColdWater.PrevValue);
             }
             else
             {
@@ -65,9 +57,11 @@ namespace CommunalPaymentsApp.MVVM.View
             }
             if ((bool)HotWater.CheckBox.IsChecked)
             {
-                hotWaterOfHeatCarrieServiceParameter = ServiceParameterCreator.CreateTariffParameter(new HotWaterParameterFactory(), HotWater.Value, HotWater.PrevValue);
-                isCorrectParams = CurMoreThanPrevParameter(HotWater.PrevValue, HotWater.Value);
+                if (!DigitValidation.TryEnterDouble(HotWater.Value.ToString(), "Горячая вода текущий") || !DigitValidation.TryEnterDouble(HotWater.PrevValue.ToString(), "Горячая вода предыдущий"))
+                    return;
+                isCorrectParams = DigitValidation.CurMoreThanPrevParameter(HotWater.PrevValue, HotWater.Value);
                 if (!isCorrectParams) return;
+                hotWaterOfHeatCarrieServiceParameter = ServiceParameterCreator.CreateTariffParameter(new HotWaterParameterFactory(), HotWater.Value, HotWater.PrevValue);
             }
             else
             {
@@ -79,13 +73,19 @@ namespace CommunalPaymentsApp.MVVM.View
             {
                 if (MainWindowVM.ElectricyTwoTariffCheckBoxIsChecked)
                 {
+                    if (!DigitValidation.TryEnterDouble(MainWindowVM.PrevElectrocityPerDay.ToString(), "Электричество за день предыдущий")
+                        || !DigitValidation.TryEnterDouble(MainWindowVM.ElectrocityPerDay.ToString(), "Электричество за день текущий"))
+                        return;
+                    isCorrectParams = DigitValidation.CurMoreThanPrevParameter(MainWindowVM.PrevElectrocityPerDay, MainWindowVM.ElectrocityPerDay);
+                    if (!isCorrectParams) return;
                     electricyPerDayServiceParameter = ServiceParameterCreator.CreateDayTariffParameter(new ElectrocityParameterFactory(), MainWindowVM.ElectrocityPerDay, MainWindowVM.PrevElectrocityPerDay);
-                    isCorrectParams = CurMoreThanPrevParameter(MainWindowVM.PrevElectrocityPerDay, MainWindowVM.ElectrocityPerDay);
-                    if (!isCorrectParams) return;
 
-                    electricyPerNightServiceParameter = ServiceParameterCreator.CreateNightTariffParameter(new ElectrocityParameterFactory(), MainWindowVM.ElectrocityPerNight, MainWindowVM.PrevElectrocityPerNight);          
-                    isCorrectParams = CurMoreThanPrevParameter(MainWindowVM.PrevElectrocityPerNight, MainWindowVM.ElectrocityPerNight);
+                    if (!DigitValidation.TryEnterDouble(MainWindowVM.PrevElectrocityPerNight.ToString(), "Электричество за ночь предыдущий") 
+                        || !DigitValidation.TryEnterDouble(MainWindowVM.ElectrocityPerNight.ToString(), "Электричество за день текущий"))
+                        return;
+                    isCorrectParams = DigitValidation.CurMoreThanPrevParameter(MainWindowVM.PrevElectrocityPerNight, MainWindowVM.ElectrocityPerNight);
                     if (!isCorrectParams) return;
+                    electricyPerNightServiceParameter = ServiceParameterCreator.CreateNightTariffParameter(new ElectrocityParameterFactory(), MainWindowVM.ElectrocityPerNight, MainWindowVM.PrevElectrocityPerNight);          
 
                     electricyServiceParameter = ServiceParameterCreator.CreateTariffParameter(new ElectrocityParameterFactory(), MainWindowVM.ElectrocityPerDay + MainWindowVM.ElectrocityPerNight,
                         MainWindowVM.PrevElectrocityPerDay + MainWindowVM.PrevElectrocityPerNight);
@@ -96,8 +96,11 @@ namespace CommunalPaymentsApp.MVVM.View
                 }
                 else
                 {
+                    if (!DigitValidation.TryEnterDouble(MainWindowVM.Electrocity.ToString(), "Электричество текущий") 
+                        || !DigitValidation.TryEnterDouble(MainWindowVM.PrevElectrocity.ToString(), "Электричество предыдущий"))
+                        return;
                     electricyServiceParameter = ServiceParameterCreator.CreateTariffParameter(new ElectrocityParameterFactory(), MainWindowVM.Electrocity, MainWindowVM.PrevElectrocity);
-                    isCorrectParams = CurMoreThanPrevParameter(MainWindowVM.PrevElectrocity, MainWindowVM.Electrocity);
+                    isCorrectParams = DigitValidation.CurMoreThanPrevParameter(MainWindowVM.PrevElectrocity, MainWindowVM.Electrocity);
                     if (!isCorrectParams) return;
                 }
             }
@@ -110,50 +113,6 @@ namespace CommunalPaymentsApp.MVVM.View
             ResultWindow resultWindow = new(serviceParameters, numberOfResidents, debt);
             resultWindow.Owner = this;
             resultWindow.ShowDialog();
-        }
-        private bool CurMoreThanPrevParameter(double prevValue, double value)
-        {
-            if (prevValue > value)
-            {
-                ShowError("Текущий параметр не может быть меньше предыдущего.");
-                return false;
-            }
-            return true;
-        }
-        private bool TryEnterInt(string str)
-        {
-            if (!CheckString(str))
-                return false;
-            else if (!int.TryParse(str, out int number))
-            {
-                ShowError("Введите целочисленное число.");
-                return false;
-            }
-            return true;
-        }
-        private bool TryEnterDouble(string str)
-        {
-            if (!CheckString(str))
-                return false;
-            else if (!double.TryParse(str, out double number))
-            {
-                ShowError("Введите нецелочисленное число.");
-                return false;
-            }
-            return true;
-        }
-        private bool CheckString(string str)
-        {
-            if (string.IsNullOrWhiteSpace(str))
-            {
-                ShowError("Вы ничего не ввели.");
-                return false;
-            }
-            return true;
-        }
-        private void ShowError(string errorMessage)
-        {
-            MessageBox.Show(errorMessage, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
         }
     }
 }
